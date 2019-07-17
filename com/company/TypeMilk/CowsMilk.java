120b57afc69e24d1f1e35227b36211abefafde23
zhdaurenkyzy <51210391+zhdaurenkyzy@users.noreply.github.com>
2019-07-18 03:01:15 +0600

    Create CowsMilk.java

---
+++ com/company/TypeMilk/CowsMilk.java
@@ -0,0 +1,10 @@
+package com.company.TypeMilk;
+
+public class CowsMilk extends Milk {
+    private String nameOfMilk="cowsMilk";
+
+    @Override
+    public String getNameOfMilk() {
+        return nameOfMilk;
+    }
+}
