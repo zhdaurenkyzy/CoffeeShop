1632eca8045bfc0b21e3e84e5ea73d6c73963a0e
zhdaurenkyzy <51210391+zhdaurenkyzy@users.noreply.github.com>
2019-07-18 03:06:04 +0600

    Create Water.java

---
+++ com/company/Water.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Water {
+    private String nameWater="water";
+    private int amountOfWater_ml;
+
+    public int getAmountOfWater_ml() {
+        return amountOfWater_ml;
+    }
+
+    public void setAmountOfWater_ml(int amountOfWater_ml) {
+        this.amountOfWater_ml = amountOfWater_ml;
+    }
+}
