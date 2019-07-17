ae84a73435dd971118db30061892f36b524381fe
zhdaurenkyzy <51210391+zhdaurenkyzy@users.noreply.github.com>
2019-07-18 03:02:25 +0600

    Create OatmealMilk.java

---
+++ com/company/TypeMilk/OatmealMilk.java
@@ -0,0 +1,10 @@
+package com.company.TypeMilk;
+
+public class OatmealMilk extends Milk {
+    private String nameOfMilk="oatmealMilk";
+
+    @Override
+    public String getNameOfMilk() {
+        return nameOfMilk;
+    }
+}
