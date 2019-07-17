fb152cf9661329ffb8117de12dc7add53867c9f3
zhdaurenkyzy <51210391+zhdaurenkyzy@users.noreply.github.com>
2019-07-18 03:04:51 +0600

    Create Arabica.java

---
+++ com/company/Arabica.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Arabica {
+    private String nameArabica;
+    private int amountOfArabica_gram;
+
+    public Arabica() {
+    }
+
+    public String getNameArabica() {
+        return nameArabica;
+    }
+
+    public void setNameArabica(String nameArabica) {
+        this.nameArabica = nameArabica;
+    }
+
+    public int getAmountOfArabica_gram() {
+        return amountOfArabica_gram;
+    }
+
+    public void setAmountOfArabica_gram(int amountOfArabica_gram) {
+        this.amountOfArabica_gram = amountOfArabica_gram;
+    }
+}
